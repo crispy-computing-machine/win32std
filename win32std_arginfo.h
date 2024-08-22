e1cc0af4fd469cfa9bca3811c741d984dcbeebbc
brittain <yup@yup.com>
2024-08-22 11:54:37 +0100

    arginfo 25

--- win32std_arginfo.h
+++ win32std_arginfo.h
@@ -10,7 +10,7 @@
 // ZEND_END_ARG_INFO()
 
 // function res_get($res_rc, $type, $name, $lang){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_res_get, 0, 4, MAY_BE_STRING|MAY_BE_BOOL, 0)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_res_get, 0, 4, MAY_BE_STRING|MAY_BE_BOOL)
 ZEND_END_ARG_INFO()
 
 // function res_list($res_rc, $type){}
@@ -26,27 +26,27 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_res_open, 0, 1, MAY_BE_LONG|MAY_
 ZEND_END_ARG_INFO()
 
 // function res_close($res_rc){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_res_close, 0, 1, _IS_BOOL)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_res_close, 0, 1, _IS_BOOL, 0)
 ZEND_END_ARG_INFO()
 
 // function res_set($module, $type, $name, $data){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_res_set, 0, 4, _IS_BOOL)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_res_set, 0, 4, _IS_BOOL, 0)
 ZEND_END_ARG_INFO()
 
 // function win_play_wav($file, $loop){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_win_play_wav, 0, 2, _IS_BOOL)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_win_play_wav, 0, 2, _IS_BOOL, 0)
 ZEND_END_ARG_INFO()
 
 // function win_beep($str){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_win_beep, 0, 1, _IS_BOOL)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_win_beep, 0, 1, _IS_BOOL, 0)
 ZEND_END_ARG_INFO()
 
 // function win_message_box($text, $type, $caption){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_win_message_box, 0, 3, IS_LONG)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_win_message_box, 0, 3, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
 // function win_create_link($file, $link, $args, $descr, $workingdir){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_win_create_link, 0, 5, _IS_BOOL)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_win_create_link, 0, 5, _IS_BOOL, 0)
 ZEND_END_ARG_INFO()
 
 // function win_browse_folder($dir, $caption){}
@@ -58,5 +58,5 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_browse_file, 0, 5, MAY_BE_ST
 ZEND_END_ARG_INFO()
 
 // function res_exists($type, $name, $lang){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_res_exists, 0, 3, _IS_BOOL)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_res_exists, 0, 3, _IS_BOOL, 0)
 ZEND_END_ARG_INFO()
