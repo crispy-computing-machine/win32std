16effe3959e2a7a575c96bef7dfa1756f21aef19
brittain <yup@yup.com>
2024-08-22 13:45:02 +0100

    num arginfo fix

--- win32std_arginfo.h
+++ win32std_arginfo.h
@@ -12,7 +12,7 @@
 // ZEND_END_ARG_INFO()
 
 // function res_get($res_rc, $type, $name, $lang){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_res_get, 0, 3, MAY_BE_STRING|MAY_BE_FALSE)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_res_get, 0, 4, MAY_BE_STRING|MAY_BE_FALSE)
 	ZEND_ARG_INFO(0, res_rc)
 	ZEND_ARG_TYPE_INFO(0, type, IS_STRING, 0)
 	ZEND_ARG_TYPE_INFO(0, name, IS_STRING, 0)
@@ -49,7 +49,7 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_res_set, 0, 4, _IS_BOOL)
 	ZEND_ARG_TYPE_INFO(0, data, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_res_exists, 0, 3, _IS_BOOL)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_res_exists, 0, 4, _IS_BOOL)
 	ZEND_ARG_INFO(0, res_rc)
 	ZEND_ARG_TYPE_INFO(0, type, IS_STRING, 0)
 	ZEND_ARG_TYPE_INFO(0, name, IS_STRING, 0)
@@ -68,14 +68,14 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_beep, 0, 1, _IS_BOOL)
 ZEND_END_ARG_INFO()
 
 //function win_message_box(){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_message_box, 0, 1, IS_LONG)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_message_box, 0, 3, IS_LONG)
 	ZEND_ARG_TYPE_INFO(0, text, IS_STRING, 0)
 	ZEND_ARG_TYPE_INFO(0, type, IS_LONG, 1)
 	ZEND_ARG_TYPE_INFO(0, caption, IS_STRING, 1)
 ZEND_END_ARG_INFO()
 
 //function win_create_link(){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_create_link, 0, 2, _IS_BOOL)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_create_link, 0, 5, _IS_BOOL)
 	ZEND_ARG_TYPE_INFO(0, file, IS_STRING, 0)
 	ZEND_ARG_TYPE_INFO(0, link, IS_STRING, 0)
 	ZEND_ARG_TYPE_INFO(0, args, IS_STRING, 1)
@@ -84,13 +84,13 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_create_link, 0, 2, _IS_BOOL)
 ZEND_END_ARG_INFO()
 
 //function win_browse_folder(){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_browse_folder, 0, 0, IS_STRING)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_browse_folder, 0, 2, IS_STRING)
 	ZEND_ARG_TYPE_INFO(0, dir, IS_STRING, 1)
 	ZEND_ARG_TYPE_INFO(0, caption, IS_STRING, 1)
 ZEND_END_ARG_INFO()
 
 //function win_browse_file(){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_browse_file, 0, 0, MAY_BE_STRING|MAY_BE_NULL)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_browse_file, 0, 5, MAY_BE_STRING|MAY_BE_NULL)
 	ZEND_ARG_TYPE_INFO(0, open, IS_LONG, 1)
 	ZEND_ARG_TYPE_INFO(0, path, IS_STRING, 1)
 	ZEND_ARG_TYPE_INFO(0, file, IS_STRING, 1)
