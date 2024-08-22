12b4084e3fb1fa38fc1f70f1fa931ddb3d71780a
brittain <yup@yup.com>
2024-08-22 11:01:41 +0100

    arginfo 16

--- win32std.c
+++ win32std.c
@@ -374,27 +374,27 @@ ZEND_FUNCTION(win_browse_file);
 // ZEND_END_ARG_INFO()
 
 // function res_get($res_rc, $type, $name, $lang){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_res_get, 0, 4, IS_MIXED, 0)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_res_get, 0, 4, IS_STRING, 1)
     ZEND_ARG_TYPE_INFO(0, res_rc, IS_RESOURCE, 0)
     ZEND_ARG_TYPE_INFO(0, type, IS_STRING, 0)
     ZEND_ARG_TYPE_INFO(0, name, IS_STRING, 0)
-    ZEND_ARG_TYPE_MASK(0, lang, MAY_BE_LONG|MAY_BE_NULL, 0)
+    ZEND_ARG_TYPE_INFO(0, lang, IS_LONG, 1)  // 1 means nullable
 ZEND_END_ARG_INFO()
 
 // function res_list($res_rc, $type){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_res_list, 0, 2, IS_ARRAY, 0)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_res_list, 0, 2, IS_ARRAY, 1)
     ZEND_ARG_TYPE_INFO(0, res_rc, IS_RESOURCE, 0)
     ZEND_ARG_TYPE_INFO(0, type, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 // function res_list_type($res_rc, $as_string){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_res_list_type, 0, 2, IS_ARRAY, 0)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_res_list_type, 0, 2, IS_ARRAY, 1)
     ZEND_ARG_TYPE_INFO(0, res_rc, IS_RESOURCE, 0)
     ZEND_ARG_TYPE_INFO(0, as_string, _IS_BOOL, 0)
 ZEND_END_ARG_INFO()
 
 // function res_open($module){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_res_open, 0, 1, IS_RESOURCE, 0)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_res_open, 0, 1, IS_RESOURCE, 1)
     ZEND_ARG_TYPE_INFO(0, module, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
@@ -445,12 +445,12 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_win_browse_folder, 0, 2, IS_STRI
 ZEND_END_ARG_INFO()
 
 // function win_browse_file($open, $path, $file, $ext, $zfilter){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_win_browse_file, 0, 5, IS_MIXED, 0)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_win_browse_file, 0, 5, IS_STRING, 1)
     ZEND_ARG_TYPE_INFO(0, open, IS_LONG, 0)
     ZEND_ARG_TYPE_INFO(0, path, IS_STRING, 0)
     ZEND_ARG_TYPE_INFO(0, file, IS_STRING, 0)
     ZEND_ARG_TYPE_INFO(0, ext, IS_STRING, 0)
-    ZEND_ARG_TYPE_MASK(0, zfilter, MAY_BE_STRING|MAY_BE_ARRAY, 0)
+    ZEND_ARG_TYPE_INFO(0, zfilter, IS_MIXED, 0)
 ZEND_END_ARG_INFO()
 
 // function res_exists($type, $name, $lang){}
