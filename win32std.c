13b9fb046f642c951e31b004ac06a7b2f50ddd4e
brittain <yup@yup.com>
2024-08-22 10:52:20 +0100

    arginfo 14

--- win32std.c
+++ win32std.c
@@ -375,88 +375,89 @@ ZEND_FUNCTION(win_browse_file);
 
 // function res_get($res_rc, $type, $name, $lang){}
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_res_get, 0, 4, MAY_BE_STRING|MAY_BE_FALSE)
-	ZEND_ARG_TYPE_INFO(0, res_rc, IS_RESOURCE, 0)
-	ZEND_ARG_TYPE_INFO(0, type, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, name, IS_STRING, 0)
-	ZEND_ARG_TYPE_MASK(0, lang, MAY_BE_LONG|MAY_BE_NULL, "")
+    ZEND_ARG_TYPE_INFO(0, res_rc, IS_RESOURCE, 0)
+    ZEND_ARG_TYPE_INFO(0, type, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, name, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, lang, MAY_BE_LONG|MAY_BE_NULL, 0)
 ZEND_END_ARG_INFO()
 
-//function res_list($res_rc, $type){}
+// function res_list($res_rc, $type){}
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_res_list, 0, 2, MAY_BE_ARRAY|MAY_BE_FALSE)
-	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, res_rc, IS_RESOURCE, 0)
-	ZEND_ARG_TYPE_INFO(0, type, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, res_rc, IS_RESOURCE, 0)
+    ZEND_ARG_TYPE_INFO(0, type, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
-//function res_list_type($res_rc, $as_string){}
+// function res_list_type($res_rc, $as_string){}
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_res_list_type, 0, 2, MAY_BE_ARRAY|MAY_BE_FALSE)
-	ZEND_ARG_TYPE_INFO(0, res_rc, IS_RESOURCE, 0)
-	ZEND_ARG_TYPE_INFO(0, as_string, _IS_BOOL, 0)
+    ZEND_ARG_TYPE_INFO(0, res_rc, IS_RESOURCE, 0)
+    ZEND_ARG_TYPE_INFO(0, as_string, _IS_BOOL, 0)
 ZEND_END_ARG_INFO()
 
-//function res_open($module){}
+// function res_open($module){}
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_res_open, 0, 1, MAY_BE_RESOURCE|MAY_BE_FALSE)
-	ZEND_ARG_TYPE_INFO(0, module, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, module, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
-//function res_close($res_rc){}
+// function res_close($res_rc){}
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_res_close, 0, 1, _IS_BOOL)
-	ZEND_ARG_TYPE_INFO(0, res_rc, IS_RESOURCE, 0)
+    ZEND_ARG_TYPE_INFO(0, res_rc, IS_RESOURCE, 0)
 ZEND_END_ARG_INFO()
 
-//function res_set($module, $type, $mnameodule, $data){}
+// function res_set($module, $type, $name, $data){}
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_res_set, 0, 4, _IS_BOOL)
-	ZEND_ARG_TYPE_INFO(0, module, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, type, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, name, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, data, IS_LONG, 0)
+    ZEND_ARG_TYPE_INFO(0, module, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, type, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, name, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, data, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
-//function win_play_wav(){}
+// function win_play_wav($file, $loop){}
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_play_wav, 0, 2, _IS_BOOL)
-	ZEND_ARG_TYPE_INFO(0, file, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, loop, _IS_BOOL, 0)
+    ZEND_ARG_TYPE_INFO(0, file, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, loop, _IS_BOOL, 0)
 ZEND_END_ARG_INFO()
 
-//function win_beep(){}
+// function win_beep($str){}
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_beep, 0, 1, _IS_BOOL)
-	ZEND_ARG_TYPE_INFO(0, str, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, str, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
-//function win_message_box(){}
+// function win_message_box($text, $type, $caption){}
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_message_box, 0, 3, IS_LONG)
-	ZEND_ARG_TYPE_INFO(0, text, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, type, IS_LONG, 0)
-	ZEND_ARG_TYPE_INFO(0, caption, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, text, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, type, IS_LONG, 0)
+    ZEND_ARG_TYPE_INFO(0, caption, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
-//function win_create_link(){}
+// function win_create_link($file, $link, $args, $descr, $workingdir){}
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_create_link, 0, 5, _IS_BOOL)
-	ZEND_ARG_TYPE_INFO(0, file, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, link, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, args, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, descr, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, workingdir, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, file, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, link, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, args, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, descr, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, workingdir, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
-//function win_browse_folder(){}
+// function win_browse_folder($dir, $caption){}
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_browse_folder, 0, 2, IS_STRING)
-	ZEND_ARG_TYPE_INFO(0, dir, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, caption, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, dir, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, caption, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
-//function win_browse_file(){}
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_browse_file, 0, 4, MAY_BE_STRING|MAY_BE_NULL)
-	ZEND_ARG_TYPE_INFO(0, open, IS_LONG, 0)
-	ZEND_ARG_TYPE_INFO(0, path, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, file, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, ext, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, zfilter, MAY_BE_STRING|MAY_BE_ARRAY, 0)
+// function win_browse_file($open, $path, $file, $ext, $zfilter){}
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_win_browse_file, 0, 5, MAY_BE_STRING|MAY_BE_NULL)
+    ZEND_ARG_TYPE_INFO(0, open, IS_LONG, 0)
+    ZEND_ARG_TYPE_INFO(0, path, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, file, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, ext, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, zfilter, MAY_BE_STRING|MAY_BE_ARRAY, 0)
 ZEND_END_ARG_INFO()
 
+// function res_exists($type, $name, $lang){}
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_res_exists, 0, 3, _IS_BOOL)
-	ZEND_ARG_TYPE_INFO(0, type, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, name, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO(0, lang, IS_LONG, 0)
+    ZEND_ARG_TYPE_INFO(0, type, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, name, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, lang, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
 /* {{{ win32std_functions[]
